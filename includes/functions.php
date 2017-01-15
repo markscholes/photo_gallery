f15a01135be5099b170d06aaf2d8f343ea2fed82
Mark Scholes <markscholes85@gmail.com>
2017-01-15 03:54:04 +0000

    initial commit

---
+++ includes/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+function strip_zeros_from_date($marked_string = "" ){
+	//remove the marked zeros
+	$no_zeros = str_replace('*0', '', $marked_string);
+	//then remove any *s 
+	$cleaned_string = str_replace('*', '', $no_zeros);
+	return $cleaned_string;
+}
+
+function redirect_to($location = NULL){
+	if ($location != NULL){
+		header("Location: {$location}");
+		exit;
+	}
+}
+
+function output_message($message=""){
+	if (!empty($message)) {
+		return "<p class = \"message\">{$message}</p>";
+	}
+}
+?>
